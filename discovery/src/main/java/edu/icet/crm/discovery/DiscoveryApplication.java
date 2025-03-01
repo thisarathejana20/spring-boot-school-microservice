6363db1f64706915c8c449a40fbda5c7f6259f03
thisaramax20 <thisarathejana20@gmail.com>
2025-03-01 08:15:19 +0530

    implement base structure for discovery server

--- discovery/src/main/java/edu/icet/crm/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/edu/icet/crm/discovery/DiscoveryApplication.java
@@ -2,7 +2,9 @@ package edu.icet.crm.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryApplication {
 
