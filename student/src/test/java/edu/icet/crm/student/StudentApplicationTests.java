4e8b34b71ba87a57985b41f557fefab9632f130e
thisaramax20 <thisarathejana20@gmail.com>
2025-03-01 08:53:09 +0530

    registered the student server to eureka discovery server

---
+++ student/src/test/java/edu/icet/crm/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.icet.crm.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
