7ffa7fdd7cfac16c4dc8581a1f5d51d79c65257c
thisaramax20 <thisarathejana20@gmail.com>
2025-03-01 14:20:26 +0530

    externalize all the microservices configurations to configServer

--- configServer/src/main/java/edu/icet/crm/configServer/ConfigServerApplication.java
+++ configServer/src/main/java/edu/icet/crm/configServer/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package edu.icet.crm.configServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
